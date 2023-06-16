12a6e60f27d17faf05f3f4ce5c5035506eaef961
himel8 <himelkhan290@gmail.com>
2023-06-16 09:25:48 +0600

    user routes successfully added

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import cors from 'cors'
 import express, { Application, Request, Response } from 'express'
+import userRoutes from './app/modules/user/users.route'
 
 const app: Application = express()
 
@@ -9,6 +10,9 @@ app.use(cors())
 //parse data
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+
+// Applicaton route
+app.use('/api/v1/user', userRoutes)
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!')
