ba6c57f908027897c08537a866855a06d6077196
himel8 <himelkhan290@gmail.com>
2024-02-05 14:32:31 +0600

    database setup successfully added

---
+++ src/server.ts
@@ -0,0 +1,23 @@
+
+
+import mongoose from "mongoose";
+import app from "./app";
+import config from "./config/index";
+
+
+async function Bootstrap() {
+  try{
+    await mongoose.connect(config.database_url as string);
+
+    app.listen(config.port, () => {
+      console.log(`Example app listening on port ${config.port}`)
+    })
+
+    console.log('Database connected successfully 🦍💨')
+  }catch(err){
+
+console.log('❌ Database not connected ')
+  }
+}
+
+Bootstrap()
