8810124b4773f62140da8d4401d112865c219b9b
himel8 <himelkhan290@gmail.com>
2024-02-06 01:14:16 +0600

    added husky lint-stage

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,17 @@
 import cors from 'cors'
 import express, { Application } from 'express'
 
-const app:Application = express()
+const app: Application = express()
 
 app.use(cors())
 
 // parser
 app.use(express.json())
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({ extended: true }))
 
 // testing
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
-
-
 
 export default app
--- src/server.ts
+++ src/server.ts
@@ -1,22 +1,18 @@
-
-
-import mongoose from "mongoose";
-import app from "./app";
-import config from "./config/index";
-
+import mongoose from 'mongoose'
+import app from './app'
+import config from './config/index'
 
 async function Bootstrap() {
-  try{
-    await mongoose.connect(config.database_url as string);
+  try {
+    await mongoose.connect(config.database_url as string)
 
     app.listen(config.port, () => {
       console.log(`Example app listening on port ${config.port}`)
     })
 
     console.log('Database connected successfully 🦍💨')
-  }catch(err){
-
-console.log('❌ Database not connected ')
+  } catch (err) {
+    console.log('❌ Database not connected ')
   }
 }
 
