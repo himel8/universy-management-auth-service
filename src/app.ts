556a775a173323b5390df4100e22973f57bbe04e
himel8 <himelkhan290@gmail.com>
2024-02-06 00:02:03 +0600

    successfully added cors and cors/type

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,15 @@
+import cors from 'cors'
 import express, { Application } from 'express'
+
 const app:Application = express()
 
+app.use(cors())
 
+// parser
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+// testing
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
